d32982c671f005f6ae9bf1867abf0742e0dd09b9
Wonho <pabang0620@naver.com>
2024-10-17 13:32:42 +0900

    asdf

--- back-end/src/controllers/randingControllers.js
+++ back-end/src/controllers/randingControllers.js
@@ -4,13 +4,26 @@ const prisma = new PrismaClient();
 // URL 코드 설정을 가져오는 컨트롤러
 const getUrlCodeSetting = async (req, res) => {
   const { urlCode } = req.params;
+  const ip =
+    (req.headers["x-forwarded-for"] || "").split(",")[0].trim() ||
+    req.connection.remoteAddress;
+
   console.log(urlCode);
   try {
     const urlCodeSetting = await prisma.url_code_setting.findFirst({
       where: { url_code: urlCode },
     });
+
+    // URL Code Setting 존재 시, click 테이블에 기록
     if (urlCodeSetting) {
       res.json(urlCodeSetting);
+      // click 테이블에 데이터 추가
+      await prisma.click.create({
+        data: {
+          url_code: urlCode,
+          ip: ip,
+        },
+      });
     } else {
       res.status(404).send("URL Code Setting not found");
     }
@@ -48,8 +61,12 @@ const incrementClickCount = async (req, res) => {
 
 const createCustomer = async (req, res) => {
   const { name, phoneNumber, date, urlCode, urlCodeSettingId } = req.body;
+  const ip =
+    (req.headers["x-forwarded-for"] || "").split(",")[0].trim() ||
+    req.connection.remoteAddress;
+
   try {
-    const isoDate = new Date(date).toISOString();
+    const isoDate = new Date(date).toISOString(); // 날짜를 ISO 형식으로 변환
 
     const newCustomer = await prisma.customor_db.create({
       data: {
@@ -58,8 +75,10 @@ const createCustomer = async (req, res) => {
         date: isoDate,
         url_code: urlCode,
         url_code_setting_id: parseInt(urlCodeSettingId),
+        ip: ip, // IP 주소를 데이터베이스에 저장
       },
     });
+
     res.status(201).json({
       message: "Customer created successfully",
       customer: newCustomer,
